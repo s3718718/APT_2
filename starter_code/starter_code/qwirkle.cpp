09d6a1b9f767102cf1bf61c178b55b8b241a6ab4
Sam Hoch <s3721859@rmit.edu.au>
2019-04-30 13:09:17 +1000

    added my name

--- starter_code/starter_code/qwirkle.cpp
+++ starter_code/starter_code/qwirkle.cpp
@@ -1,51 +1,58 @@
 
+// Sam Hoch, s3721859
 #include "LinkedList.h"
 #include "QwirkleMenu.h"
 #include <iostream>
 
-#define EXIT_SUCCESS    0
+#define EXIT_SUCCESS 0
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 
-int main(void) {
-   // LinkedList* list = new LinkedList();
-   // delete list;
+int main(void)
+{
+  // LinkedList* list = new LinkedList();
+  // delete list;
 
-   std::string menuInputs[4] = {"1", "2", "3", "4"};
-   std::string menuInput = "";
+  std::string menuInputs[4] = {"1", "2", "3", "4"};
+  std::string menuInput = "";
 
-   while(menuInput != menuInputs[3]) {
-      cout << "Welcome to Qwirkle!" << endl;
-      cout << "-------------------" << endl;
-      cout << "Menu" << endl;
-      cout << "----" << endl;
-      cout << "1. New Game" << endl;
-      cout << "2. Load Game" << endl;
-      cout << "3. Show student information" << endl;
-      cout << "4. Quit" << endl;
-      
-      cin >> menuInput;
+  while (menuInput != menuInputs[3])
+  {
+    cout << "Welcome to Qwirkle!" << endl;
+    cout << "-------------------" << endl;
+    cout << "Menu" << endl;
+    cout << "----" << endl;
+    cout << "1. New Game" << endl;
+    cout << "2. Load Game" << endl;
+    cout << "3. Show student information" << endl;
+    cout << "4. Quit" << endl;
 
-      if(menuInput == menuInputs[0]) {
-      cout << "Starting a New Game" << endl;      
+    cin >> menuInput;
+
+    if (menuInput == menuInputs[0])
+    {
+      cout << "Starting a New Game" << endl;
       cout << "Enter a name for player 1" << endl;
       // cin >> player1Name;
       // player1Name = to_upper(player1Name);
       // cout << player1Name << endl;
-      }
-      else if(menuInput == menuInputs[1]) {
-         cout << "2 was selected" << endl;
-      }
-      else if(menuInput == menuInputs[2]) {
-         cout << "3 was selected" << endl;
-      }
-      else if(menuInput == menuInputs[3]) {
-         cout << "Goodbye" << endl;
-      }
+    }
+    else if (menuInput == menuInputs[1])
+    {
+      cout << "2 was selected" << endl;
+    }
+    else if (menuInput == menuInputs[2])
+    {
+      cout << "3 was selected" << endl;
+    }
+    else if (menuInput == menuInputs[3])
+    {
+      cout << "Goodbye" << endl;
+    }
 
-      cout << "Invalid Input. Try again" << endl;
-   }
-   return EXIT_SUCCESS;
+    cout << "Invalid Input. Try again" << endl;
+  }
+  return EXIT_SUCCESS;
 }
