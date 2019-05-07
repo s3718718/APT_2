7730147b4b6f1ac06e1e55de2aff5123f3a8adc9
Sam Hoch <s3721859@rmit.edu.au>
2019-05-07 14:50:08 +1000

    does this even show up?

--- starter_code/starter_code/Bag.h
+++ starter_code/starter_code/Bag.h
@@ -1,13 +1,14 @@
 #include <cstdlib>
 #include "Tile.h"
 
-class Bag{
-    public:
-        Bag();
-        int size();
-        Tile pullTile();
-        void print();
+class Bag
+{
+  public:
+    Bag();
+    int size();
+    Tile pullTile();
+    void print();
 
-    private:
-        // Make ordered linked lists of tiles
+  private:
+    // Make ordered linked lists of tiles
 };
