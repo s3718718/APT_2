8fa25e7ee98a1afc59a9395c8d7e5708de7e64f5
abir1997 <s3677701@student.rmit.edu.au>
2019-05-07 20:37:38 +1000

    Bag v1.1
    fixed construcot

--- starter_code/starter_code/Bag.cpp
+++ starter_code/starter_code/Bag.cpp
@@ -1,26 +1,20 @@
 #include <cstdlib>
 #include "Bag.h"
-#include "Tile.h"
 #include <iostream>
+
+using std::cout;
 
 Bag::Bag()
 {
-    int tileArr[1];
-    tileArr[0] = 1;
-    std::cout << tileArr[0] << Tile::colours[0];
-    //int iterator = 0;
-    // for (const Colour &colour : Tile::colours)
-    // {
-    //     for (const Shape &shape : Tile::shapes)
-    //     {
-    //         std::cout << shape << colour;
-    //         //may need to allocate to the stack to avoid overwrite
-    //         //Tile tile = Tile(colour, shape);
-    //         tileArr[iterator] = iterator;
-
-    //         //TODO : add to linkedList, then shuffle
-    //     }
-    // }
+    tileList = new LinkedList();
+      // add tile of every shape/color combo available
+    for ( Colour &color : colours){     //TODo : Fix arrays
+        for ( Shape &shape : shapes) {
+            Tile tile = Tile(shape, color);
+            tileList->add(tile);
+        }
+    }
+    tileList->printList();
 }
 
 // For debugging purposes. MUST Remove from final impl
@@ -28,3 +22,4 @@ void Bag::print()
 {
     std::cout << "bagprint";
 }
+
--- starter_code/starter_code/Bag.h
+++ starter_code/starter_code/Bag.h
@@ -1,14 +1,18 @@
 #include <cstdlib>
 #include "Tile.h"
-
+#include "LinkedList.h"
 class Bag
 {
 public:
+    Colour colours[6] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE};
+    Shape shapes[6] = {CIRCLE, STAR_4, DIAMOND, SQUARE, STAR_6, CLOVER};
   Bag();
   int size();
   Tile pullTile();
   void print();
+  LinkedList shuffle();
+  bool isEmpty();
 
 private:
-  // Make ordered linked lists of tiles
+  LinkedList* tileList;
 };
